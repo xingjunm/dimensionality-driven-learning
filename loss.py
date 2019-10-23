c652f6e9127938dfe05ee2c7b195aa9aab9d73d9
Xingjun Ma <xingjun.ma@unimelb.edu.au>
2019-10-23 22:47:54 +1100

    Update loss.py
    add the symmetric cross entropy loss

--- loss.py
+++ loss.py
@@ -3,6 +3,25 @@ from keras import backend as K
 import tensorflow as tf
 
 
+def symmetric_cross_entropy(alpha, beta):
+    """
+    Symmetric Cross Entropy: 
+    ICCV2019 "Symmetric Cross Entropy for Robust Learning with Noisy Labels" 
+    https://arxiv.org/abs/1908.06112
+    """
+    def loss(y_true, y_pred):
+        y_true_1 = y_true
+        y_pred_1 = y_pred
+
+        y_true_2 = y_true
+        y_pred_2 = y_pred
+
+        y_pred_1 = tf.clip_by_value(y_pred_1, 1e-7, 1.0)
+        y_true_2 = tf.clip_by_value(y_true_2, 1e-4, 1.0)
+
+        return alpha*tf.reduce_mean(-tf.reduce_sum(y_true_1 * tf.log(y_pred_1), axis = -1)) + beta*tf.reduce_mean(-tf.reduce_sum(y_pred_2 * tf.log(y_true_2), axis = -1))
+    return loss
+
 def cross_entropy(y_true, y_pred):
     return K.categorical_crossentropy(y_true, y_pred)
 
